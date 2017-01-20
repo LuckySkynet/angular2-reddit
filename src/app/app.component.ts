8f915bbc351fbe53d4cf0a924431eefab9da8982
LuckySkynet <skynet_s@163.com>
2017-01-20 21:55:45 +0800

    实现新增删除过渡效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from './reddit-article/article';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'reddit',
   templateUrl: './app.component.html',
@@ -11,42 +13,7 @@ export class AppComponent {
 
   articleForm: FormGroup;
   articles: Article[];
-
-  constructor(private fb: FormBuilder) {
-    this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1),
-    ];
-  }
-
-  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
-    this.articles.push(new Article(title.value, link.value));
-    title.value = '';
-    link.value = '';
-  }
-
-  sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
-  }
-
-  ngOnInit() {
-    this.buildForm();
-  }
-
-  buildForm() {
-    const regExp: string = '((https|http)\:\/\/) ((([0-9]{1,3}\.){3}[0-9]{1,3}) | (([0-9a-z]+\.)*)) ([a-z]{2,6}) (:[0-9]{1,4})? (\/[a-z]+)*';
-    this.articleForm = this.fb.group({
-      'title': [this.articles.forEach(article => article.title), Validators.compose([
-        Validators.required,
-        Validators.minLength(4)]
-      )],
-      'link': [this.articles.forEach(article => article.link), Validators.compose([
-        Validators.required,
-        Validators.pattern(regExp)]
-      )]
-    })
-  }
+  active: boolean = true;
 
   validationMessages = {
     'title': {
@@ -59,9 +26,63 @@ export class AppComponent {
     }
   };
 
-  formError = {
-    'title':'',
-    'link':''
+  formErrors = {
+    'title': '',
+    'link': ''
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle() {
+    //this.articleForm.value 能够获取该formGroup中的formControl对象的值
+    this.articles.push(new Article(this.articleForm.value['title'], this.articleForm.value['link']));
+    this.articleForm.reset();
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes)
+  }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    //注意：正则表达式不能带有空格
+    const regExp: string = '((https|http)\:\/\/)((([0-9]{1,3}\.){3}[0-9]{1,3})|(([0-9a-z]+\.)*))([a-z]{2,6})(:[0-9]{1,4})?(\/[a-z]+)*';
+    this.articleForm = this.fb.group({
+      'title': [this.articles.forEach(article => article.title), Validators.compose([
+        Validators.required,
+        Validators.minLength(4)]
+      )],
+      'link': [this.articles.forEach(article => article.link), Validators.compose([
+        Validators.required,
+        Validators.pattern(regExp)]
+      )]
+    })
+    this.articleForm.valueChanges.subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.articleForm) { return; }
+    const form = this.articleForm;
+    for (const field in this.formErrors) {
+      //重置错误消息
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
 }
--- src/app/reddit-article/reddit-article.component.ts
+++ src/app/reddit-article/reddit-article.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, Host, forwardRef, Inject } from '@angular/core';
+import { Component, OnInit, Input, Host, forwardRef, Inject, ViewChild, ElementRef } from '@angular/core';
 import { Article } from './article';
 import { AppComponent } from '../app.component';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'reddit-article',
@@ -12,16 +14,18 @@ import { AppComponent } from '../app.component';
 })
 export class RedditArticleComponent implements OnInit {
 
-  @Input()
-  article: Article;
+  @Input() article: Article;
+
+  @ViewChild('closeArt') closeArt: ElementRef;
 
   articles: Article[];
 
-  constructor( @Host() @Inject(forwardRef(() => AppComponent)) app: AppComponent ) {
+  constructor( @Host() @Inject(forwardRef(() => AppComponent)) app: AppComponent) {
     this.articles = app.articles;
   }
 
   ngOnInit() {
+    jQuery(this.closeArt.nativeElement.parentElement).transition('fade in');
   }
 
   voteUp() {
@@ -35,7 +39,12 @@ export class RedditArticleComponent implements OnInit {
   removeArticle(article: Article) {
     const index = this.articles.indexOf(article);
     if (index > -1) {
-      this.articles.splice(index, 1);
+      //angular2使用jQuery：首先声明jQuery对象
+      //1、导入ElementRef, ViewChild模块
+      //2、通过@ViewChild('close') close: ElementRef的方式传入模板引用变量，创建元素引用对象
+      //3、调用对象的nativeElement获取原生的元素，即可使用js方法
+      jQuery(this.closeArt.nativeElement.parentElement).transition('fade out');
+      setTimeout(() => this.articles.splice(index, 1), 500);
     }
   }
 
